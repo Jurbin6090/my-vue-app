ed359bf244a63c4a6f4f15f5d90c3207981e1c5b
Justin <jurbin6090@gmail.com>
2018-05-16 15:40:26 -0500

    Set up to pass to children component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <input v-model="username" />
+        <input v-model="password" />
+        <button @click="login()">Login</button>
+        <HelloWorld v-bind:msg={isLoggedIn}></HelloWorld>
+
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
+    const isLoggedIn = false
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+
+
+    export default {
+        methods: {
+            login(){
+
+                //Uncomment the following
+
+//              fetch('http://localhost:8080/login',{
+//                  method: 'POST',
+//                  headers:{
+//                      'Content-Type': 'application/json',
+//                  },
+//                  body: JSON.stringify({
+//                      username: this.username,
+//                      password: this.password
+//                  })
+//              })
+//                  .then(response=> response.json())
+//                  .then(isSuccess=> this.isLoggedIn = isSuccess)
+
+                //Remove the following
+
+                if(this.username === 'Justin' && this.password === 'test')
+                    this.isLoggedIn = true
+                else
+                    this.isLoggedIn = false
+            }
+        },
+        name: 'app',
+        components: {
+            HelloWorld
+        },
+        data: function () {
+            return {
+                username: '',
+                password: '',
+                isLoggedIn: false
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
